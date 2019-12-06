2e2ce66368e57b03655e912a9359a1557cdc335c
wassimramy <wassimramy@me.com>
2019-12-06 17:42:32 -0600

    Fixed hopefully SRT, TXT, and Audio files retrieval issues

--- app/src/main/java/com/wrkhalil/learningthroughlistening/BaseApplication.java
+++ app/src/main/java/com/wrkhalil/learningthroughlistening/BaseApplication.java
@@ -1,12 +1,10 @@
 package com.wrkhalil.learningthroughlistening;
 
 import android.app.Application;
-import android.app.ProgressDialog;
 import android.content.Context;
 
 public class BaseApplication extends Application {
     private static Context context;
-    public static ProgressDialog progressDialog;
 
     public void onCreate() {
         super.onCreate();
